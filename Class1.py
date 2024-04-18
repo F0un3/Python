d3d7bf31ef3772a0d3dd8d3ffb6bd6b59a142654
F0un3 <fleming3.william@gmail.com>
2024-04-18 00:53:23 -0400

    Update Class1.py
    went over class variables and how they work

--- Class1.py
+++ Class1.py
@@ -1,32 +1,38 @@
 class members:
     
+    raise_ammount = 1.04
+    num_of_mem = 0
+
     # Takes the first, last, and pay and assignes it everytime the class is called
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
         self.pay = pay
+
+        # This is a class variable
+        # The num_of_mem will increase by 1 every time the class is called on an object
+        members.num_of_mem += 1
+    # print(members.num_of_mem)
     
     # After given the full name calling this will return the full name
     # This is a method
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
+    
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_ammount)
 
 #mem1 and mem2 are objects of the class members
 # Assign both members with names and pay
 mem1 = members("John", "Smith", 50000)
 mem2 = members("Amy", "Williams", 60000)
 
-# print(mem1)
-# print(mem2)
+print(members.num_of_mem)
 
-#to access the class members, we use the dot operator
-# print(mem1.first)
-# print(mem2.last)
+# mem1.raise_ammount = 1.05
 
-#This does the same as the other code below just using the class name
-print(members.fullname(mem1))
-print(members.fullname(mem2))
+# # print(mem1.__dict__)
 
-
-# print(mem1.fullname())
-# print(mem2.fullname())
+# print(members.raise_ammount)
+# print(mem1.raise_ammount)
+# print(mem2.raise_ammount)
